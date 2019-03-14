c3fa063b894993ba7a2f7224c3f2a90019e4bab0
EdenChanIy <chenjh246@mail2.sysu.edu.cn>
2019-03-14 22:59:55 +0800

    简单异步请求

--- src/main/AsyncGet.java
+++ src/main/AsyncGet.java
@@ -1,4 +1,30 @@
 package main;
 
+import okhttp3.*;
+
+import java.io.IOException;
+
 public class AsyncGet {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        Request request = new Request.Builder()
+                .url("http://www.baidu.com")
+                .build();
+
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.println(response.body().string());
+            }
+        });
+
+
+
+    }
 }
